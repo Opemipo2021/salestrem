8b47e4ed0a8a92085628c5e34d739cfbf6881194
Opemipo2021 <omoimole2020@gmail.com>
2025-01-24 00:21:17 +0100

    UPDATED PRISMA

---
+++ src/app/(protected)/dashboard/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {}
+
+const Page = async (props: Props) => {
+    // Server Actions Dashboard Page
+    //200 || 201 
+
+
+
+
+  return  <div>Page</div>
+}
+
+export default Page
